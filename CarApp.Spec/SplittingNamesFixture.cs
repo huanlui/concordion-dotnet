a5651b8e67b7551798b33f807ef83a1e452041f7
huanlui <juanluisgarcialopez@outlook.es>
2017-12-03 15:09:50 +0100

    Tenía problema con el adaptador de NUnit 2. Así que he tenido que bajarme el proyecto Concordion.NET y recompilarlo para NUnit 3. Después, me he importado las dlls generadas manualmente. Lo suyo sería tener nuestro propio fork de Concordion en nuestro servidor nuget y referenciar ahí.
    Por otro lado, me he instalado en el proyecto el apapter para nunit3 para que me vea los test.
    
    Para un html que esté en /raizdelproyecto/test/MiTest.hmtl, tengo que crear una clase MiTestFixture.cs que esté en el namespace "namespacepordefectodelproyecto.test".
    
    Además, debe estar anotada con TestFixture y extender a ExecutableSpecification:
    
      [TestFixture]
        public class SplittingNamesFixture : ExecutableSpecification
    
    Así funcionarán los test, y me creará una salida en  :
     C:/Users/juanl/AppData/Local/Temp/CarApp/Spec/SplittingNames.html

---
+++ CarApp.Spec/SplittingNamesFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Concordion.Runners.NUnit;
+
+namespace CarApp.Spec
+{
+  [TestFixture]
+    public class SplittingNamesFixture : ExecutableSpecification
+    {
+        public Result Split(string fullName)
+        {
+            Result result = new Result();
+            string[] words = fullName.Split(' ');
+            result.firstName = words[0];
+            result.lastName = words[1];
+            return result;
+        }
+    }
+
+    public class Result
+    {
+        public string firstName;
+        public string lastName;
+    }
+}
